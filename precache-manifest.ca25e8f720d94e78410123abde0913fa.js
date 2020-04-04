90b85912ff792e36e88b3dababae62363c5b59c8
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-04 21:42:34 +0200

    Updates

---
+++ precache-manifest.ca25e8f720d94e78410123abde0913fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30336faa9ef5cf628c0eb4e701fc1540",
+    "url": "/cancionesapi/index.html"
+  },
+  {
+    "revision": "f5cafdcda455a9327f71",
+    "url": "/cancionesapi/static/css/main.ff9c8e57.chunk.css"
+  },
+  {
+    "revision": "1034fb02f951e41c33b7",
+    "url": "/cancionesapi/static/js/2.aa23f0cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cancionesapi/static/js/2.aa23f0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5cafdcda455a9327f71",
+    "url": "/cancionesapi/static/js/main.30158909.chunk.js"
+  },
+  {
+    "revision": "e2e115cec4aee1e2f46c",
+    "url": "/cancionesapi/static/js/runtime-main.885cd675.js"
+  }
+]);
